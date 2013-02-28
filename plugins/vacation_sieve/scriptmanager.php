d8b96a7ef73b37bc2ebdf05ca93abaa3e2189438
Andre Rodier <andre.rodier@gmail.com>
2013-02-28 23:24:02 +0000

    Fixed vacation plugin
    Hour selection in the vacation plugin is not working. Writing only the date for now.

--- plugins/vacation_sieve/scriptmanager.php
+++ plugins/vacation_sieve/scriptmanager.php
@@ -42,12 +42,20 @@ class ScriptManager
             # Build conditions
             $startDate = preg_replace('#(\d\d)/(\d\d)/(\d\d\d\d)#','$3-$2-$1',$params['start']);
             $endDate = preg_replace('#(\d\d)/(\d\d)/(\d\d\d\d)#','$3-$2-$1',$params['end']);
+
+            $execBlock .= sprintf('if allof (currentdate :value "gt" "date" "%s",'.
+                                           ' currentdate :value "lt" "date" "%s")'."\n",
+                $startDate,
+                $endDate);
+
+            /* This is not working, and need to be reworked
             $execBlock .= sprintf('if allof (allof (currentdate :value "gt" "date" "%s", currentdate :value "gt" "hour" "%02d"),'.
                                            ' allof (currentdate :value "lt" "date" "%s", currentdate :value "lt" "hour" "%02d"))'."\n",
                 $startDate,
                 $params['starttime'],
                 $endDate,
                 $params['endtime']);
+            */
 
             # Start to build the script
             $execBlock .= "{\n    vacation\n";
