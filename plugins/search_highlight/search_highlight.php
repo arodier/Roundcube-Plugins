31effdfa39dcac66283d1adfb5ec8d51dbf10645
Andre Rodier <andre.rodier@gmail.com>
2013-03-08 21:28:09 +0000

    Fixed HTML text highlighter
    Used a good example of php code to hightlight searched words without corrupting HTML tags

--- plugins/search_highlight/search_highlight.php
+++ plugins/search_highlight/search_highlight.php
@@ -1,5 +1,32 @@
 <?php
 
+
+/**
+ * Perform a simple text replace
+ * This should be used when the string does not contain HTML
+ * (off by default)
+ */
+define('STR_HIGHLIGHT_SIMPLE', 1);
+ 
+/**
+ * Only match whole words in the string
+ * (off by default)
+ */
+define('STR_HIGHLIGHT_WHOLEWD', 2);
+ 
+/**
+ * Case sensitive matching
+ * (off by default)
+ */
+define('STR_HIGHLIGHT_CASESENS', 4);
+ 
+/**
+ * Overwrite links if matched
+ * This should be used when the replacement string is a link
+ * (off by default)
+ */
+define('STR_HIGHLIGHT_STRIPLINKS', 8);
+ 
 /**
  * Search Highlight
  *
@@ -13,6 +40,7 @@ class search_highlight extends rcube_plugin
 {
   public $task = 'mail';
   private $map;
+  private $styles;
 
   function init()
   {
@@ -21,17 +49,8 @@ class search_highlight extends rcube_plugin
 
     $this->map = array();
 
-    $this->include_script('search_highlight.js');
-  }
-
-  function store_query($args)
-  {
-    setcookie('search_query', $_POST['query']);
-  }
-
-  function highlight($args)
-  {
-    $styles = array(
+    # Same styles as google highlighted words
+    $this->styles = array(
       'color: #000; background-color: #ff6;',
       'color: #000; background-color: #aff;',
       'color: #000; background-color: #9f9;',
@@ -44,19 +63,100 @@ class search_highlight extends rcube_plugin
       'color: #fff; background-color: #909;',
     );
 
-    $words = explode(' ', $_COOKIE['search_query']);
-    $wn = 0;
+    $this->include_script('search_highlight.js');
+  }
+
+  function store_query($args)
+  {
+    $query = $_POST['query'];
+    setcookie('search_query', $query);
+  }
+
+  function highlight($args)
+  {
+    $return = array();
+    $body = $args['body'];
+    $isHtml = $args['type'] != 'plain';
+
+    $words = array_filter(explode(' ', $_COOKIE['search_query']), array($this, 'filter_words'));
+    if ( empty($words) ) return;
+
+    $sn = 0;
     foreach ( $words as $word ) {
-      if ( $word == 'AND' || $word == 'OR' || empty($word) ) continue;
-      $this->map[$word] = html::span(array('style' => $styles[$wn]), $word);
-      $wn++;
+
+      $style = $this->styles[$sn];
+      $replace = sprintf('<span style="%s">\1</span>', $style);
+      $body = $this->highlight_html($body, $word, null, $replace);
+      $sn++;
     }
 
-    if (  count($this->map) == 0 ) return;
+    $return['body'] = $body;
 
-    if ( $args['type'] == 'plain' )
-      return array('body' => str_ireplace(array_keys($this->map), array_values($this->map), $args['body']));
-    else
-      return array('body' => str_ireplace(array_keys($this->map), array_values($this->map), $args['body']));
+    return $return;
+  }
+
+  private function filter_words($word)
+  {
+    $word = strtoupper($word);
+    if ( !empty($word) && ($word != 'OR') && ($word != 'AND') )
+      return $word;
+  }
+
+
+  /**
+   * Highlight a string in text without corrupting HTML tags
+   *
+   * @author      Aidan Lister <aidan@php.net>
+   * @version     3.1.1
+   * @link        http://aidanlister.com/2004/04/highlighting-a-search-string-in-html-text/
+   * @param       string          $text           Haystack - The text to search
+   * @param       array|string    $needle         Needle - The string to highlight
+   * @param       bool            $options        Bitwise set of options
+   * @param       array           $highlight      Replacement string
+   * @return      Text with needle highlighted
+   */
+  function highlight_html($text, $needle, $options = null, $highlight = null)
+  {
+      // Default highlighting
+      if ($highlight === null) {
+          $highlight = '<strong>\1</strong>';
+      }
+   
+      // Select pattern to use
+      if ($options & STR_HIGHLIGHT_SIMPLE) {
+          $pattern = '#(%s)#';
+          $sl_pattern = '#(%s)#';
+      } else {
+          $pattern = '#(?!<.*?)(%s)(?![^<>]*?>)#';
+          $sl_pattern = '#<a\s(?:.*?)>(%s)</a>#';
+      }
+   
+      // Case sensitivity
+      if (!($options & STR_HIGHLIGHT_CASESENS)) {
+          $pattern .= 'i';
+          $sl_pattern .= 'i';
+      }
+   
+      $needle = (array) $needle;
+      foreach ($needle as $needle_s) {
+          $needle_s = preg_quote($needle_s);
+   
+          // Escape needle with optional whole word check
+          if ($options & STR_HIGHLIGHT_WHOLEWD) {
+              $needle_s = '\b' . $needle_s . '\b';
+          }
+   
+          // Strip links
+          if ($options & STR_HIGHLIGHT_STRIPLINKS) {
+              $sl_regex = sprintf($sl_pattern, $needle_s);
+              $text = preg_replace($sl_regex, '\1', $text);
+          }
+   
+          $regex = sprintf($pattern, $needle_s);
+          $text = preg_replace($regex, $highlight, $text);
+      }
+   
+      return $text;
   }
 }
+
